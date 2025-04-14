6bd0bbb91aa01158d4f5834bd4a12948ca4c0031
Eddie C. <edimc2000@gmail.com>
2025-04-13 23:49:32 -0500

    Initial commit, created MH and unit test

---
+++ MHrandomNumberGenerator1To10_20times.js
@@ -0,0 +1,5 @@
+import { randomNumberGenerator } from "./mathhelper.cjs"
+
+
+let rand = randomNumberGenerator(1,10)
+console.log(rand)
---
+++ mathhelper.cjs
@@ -0,0 +1,20 @@
+/**
+ * Math Helper by Eddie C. randomNumberGenerator
+ * @param {*} start an integer 
+ * @param {*} end an integer 
+ * @returns a number between start and end, start and end numbers are included
+ */
+
+function randomNumberGenerator (start, end) {
+    return Math.floor(Math.random() * (end - start + 1 ) + start);
+}
+
+
+
+
+module.exports = {
+    randomNumberGenerator,
+
+
+} 
+
